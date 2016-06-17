65876cf34602ba2515e2465963bfdfa1acd99fbc
Dan Rabinowitz <djr@DanielRabinowitz.com>
2016-06-16 23:04:19 -0600

    Add initialize method for Weekly

--- lib/relative_time/weekly.rb
+++ lib/relative_time/weekly.rb
@@ -1,4 +1,6 @@
 module RelativeTime
   class Weekly
+    def initialize(day_of_week: 0, time_of_day: TimeOfDay.new)
+    end
   end
 end
--- spec/relative_time/weekly_spec.rb
+++ spec/relative_time/weekly_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 describe RelativeTime::Weekly do
   let(:day_of_week) { 1 }
-  let(:time_of_day) { TimeOfDay.new(hour: hour, minute: minute, second: second) }
+  let(:hour) { 13 }
+  let(:minute) { 47 }
+  let(:second) { 12.3 }
+  let(:time_of_day) { RelativeTime::TimeOfDay.new(hour: hour, minute: minute, second: second) }
 
   it "accepts a day_of_week and time_of_day" do
-    skip
     RelativeTime::Weekly.new(day_of_week: day_of_week, time_of_day: time_of_day)
   end
 end
