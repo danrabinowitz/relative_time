9d75c4fe601821ff912616ef7aade586bac65360
Dan Rabinowitz <djr@DanielRabinowitz.com>
2016-06-16 23:09:33 -0600

    Require arguments to Weekly.new to be valid

--- lib/relative_time/weekly.rb
+++ lib/relative_time/weekly.rb
@@ -1,6 +1,18 @@
 module RelativeTime
   class Weekly
     def initialize(day_of_week: 0, time_of_day: TimeOfDay.new)
+      @day_of_week = day_of_week
+      @time_of_day = time_of_day
+      validate!
+    end
+
+    private
+
+    attr_reader :day_of_week, :time_of_day
+
+    def validate!
+      raise(TypeError, "day_of_week must be an integer in 0..6") unless day_of_week.is_a?(Integer) && (0..6).include?(day_of_week)
+      raise(TypeError, "time_of_day must be a RelativeTime::TimeOfDay") unless time_of_day.is_a?(RelativeTime::TimeOfDay)
     end
   end
 end
--- spec/relative_time/weekly_spec.rb
+++ spec/relative_time/weekly_spec.rb
@@ -6,8 +6,27 @@ describe RelativeTime::Weekly do
   let(:minute) { 47 }
   let(:second) { 12.3 }
   let(:time_of_day) { RelativeTime::TimeOfDay.new(hour: hour, minute: minute, second: second) }
+  let(:weekly) { RelativeTime::Weekly.new(day_of_week: day_of_week, time_of_day: time_of_day) }
 
-  it "accepts a day_of_week and time_of_day" do
-    RelativeTime::Weekly.new(day_of_week: day_of_week, time_of_day: time_of_day)
+  describe ".new" do
+    it "accepts a day_of_week and time_of_day" do
+      expect(weekly).to be_a(RelativeTime::Weekly)
+    end
+
+    describe "day_of_week is not an integer" do
+      let(:day_of_week) { 1.5 }
+
+      it "raises an exception" do
+        expect{weekly}.to raise_error(TypeError)
+      end
+    end
+
+    describe "time_of_day is not a TimeOfDay" do
+      let(:time_of_day) { "string" }
+
+      it "raises an exception" do
+        expect{weekly}.to raise_error(TypeError)
+      end
+    end
   end
 end
